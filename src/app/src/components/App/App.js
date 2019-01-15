823c2862561c7db8517e60563b870c9854cfce09
Artyom <makeiTEasysoft@gmail.com>
2019-01-15 22:51:19 +0300

    added default components

--- src/app/src/components/App/App.js
+++ src/app/src/components/App/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import logo from '../../assets/img/logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import './App.scss';
+import Login from "../Login/Login";
+import NoMatch from "../NoMatch/NoMatch";
+import Profile from "../Profile/Profile";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>./app/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Profile} />
+          <Route exact path="/login" component={Login} />
+
+
+          {/*<Route exact path="/" component={Home} />*/}
+          {/*<Route path="/about" component={About} />*/}
+          {/*<Route path="/topics" component={Topics} />*/}
+          <Route component={NoMatch} />
+        </div>
+      </Router>
     );
   }
 }
