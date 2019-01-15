a522e2acb41aa55387fa6bbdb10132883cca2acd
Artyom <makeiTEasysoft@gmail.com>
2019-01-15 22:05:19 +0300

    changed file structure

--- src/api/app.js
+++ src/api/app.js
@@ -38,5 +38,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('app now listening for requests on port 3000');
+    console.log('App now listening for requests on port 3000');
 });
---
+++ src/app/src/components/Login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Login.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="Login">
+            </div>
+        );
+    }
+}
+
+export default App;
