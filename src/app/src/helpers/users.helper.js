36be577d4c285ed6274ad6a6c8c2e352df686d23
Artyom <makeiTEasysoft@gmail.com>
2019-02-07 22:58:03 +0300

    fixed creation of user observable

--- src/app/src/helpers/users.helper.js
+++ src/app/src/helpers/users.helper.js
@@ -3,21 +3,33 @@ import request from './request';
 
 export default class usersHelper {
   constructor(){
-    this.usersObserv = new BehaviorSubject(null);
+
   }
 
   static getCurrentUserObserv(){
     return this.usersObserv;
   };
 
- static logIn(){
+  static logIn(){
     return request.GET('test')
       .then( result => {
-        this.usersObserv.next(result);
+        this.getUserObservable().next(result);
       })
       .catch( error => {
         console.error(error);
       })
+  }
+
+  static getUserObservable(){
+    if(this.usersObserv){
+      return this.usersObserv;
+    } else {
+      return this.createUserObservable();
+    }
+  }
+
+  static createUserObservable(){
+    return this.usersObserv = new BehaviorSubject(null);
   }
 };
 
